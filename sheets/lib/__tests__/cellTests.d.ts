2ab2237f167032c8e61eff9a09fbd13cf927fbeb
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-02-13 16:10:26 +0100

    Removed build files

--- sheets/lib/__tests__/cellTests.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=cellTests.d.ts.map
--- sheets/lib/consoleRenderer.d.ts
+++
@@ -1,3 +0,0 @@
-import { Table } from ".";
-export declare function renderTable(table: Table): string;
-//# sourceMappingURL=consoleRenderer.d.ts.map
