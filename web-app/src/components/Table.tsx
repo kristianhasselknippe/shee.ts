874e5e86b1093d9e1847e31ab60bbc65416bc350
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-02-13 16:09:33 +0100

    Started creating api for drawing tables using the canvas api

--- web-app/src/components/Table.tsx
+++ web-app/src/components/Table.tsx
@@ -1,59 +1,33 @@
-import * as React from 'react'
 import * as Sheets from 'sheets'
 import "./Table.css"
+import { createRenderingInfo } from 'sheets'
 
 interface TableProps {
 	table: Sheets.Table
+	cellHeight: number
+	renderingContext: CanvasRenderingContext2D
+	offset: {
+		x: number,
+		y: number
+	}
 }
 
-export class SheetsTable extends React.Component<TableProps> {
-
-	renderTable = () => {
-		let ret = []
-		for (let y = 0; y < this.props.table.height; y++) {
-			const row: any[] = []
-			for (let x = 0; x < this.props.table.width; x++) {
-				const val = this.props.table.getCellValue(x,y)
-				row.push(val)
-			}
-			ret.push(row)
+export function renderTable(props: TableProps) {
+	const ctx = props.renderingContext
+	const fontSize = 30
+	ctx.font = `${fontSize}px Arial`
+	const { cellValues, cellWidths } = createRenderingInfo(props.table)
+	let yPos = 50
+	for (let y = 0; y < props.table.height; y++) {
+		let xPos = 50
+		for (let x = 0; x < props.table.width; x++) {
+			ctx.fillText(cellValues[y * props.table.width + x].toString(), xPos + props.offset.x, yPos + props.offset.y)
+			xPos += cellWidths[x] * 30
 		}
-		const retTable = (
-			<table className="Table">
-				<tbody>
-					{
-						ret.map(row => {
-							return (
-								<tr>
-									{
-										row.map(cellValue => {
-											return (
-												<th>
-													<p>
-														{cellValue}
-													</p>
-												</th>
-											)
-										})
-									}
-								</tr>
-							)
-						})
-					}
-				</tbody>
-			</table>
-		)
-
-		return retTable
-	}
-
-	render() {
-		const table = this.props.table
-		return (
-			<div className="Table">
-				<p>{table.name}</p>
-				{this.renderTable()}
-			</div>
-		)
+		ctx.moveTo(50 + props.offset.x, yPos)
+		ctx.lineTo(xPos + props.offset.x, yPos)
+		ctx.stroke()
+		console.log(`50,${yPos} - ${xPos},${yPos}`)
+		yPos += props.cellHeight * 30
 	}
 }
--- web-app/src/components/Workspace.tsx
+++ web-app/src/components/Workspace.tsx
@@ -1,15 +1,46 @@
 import { Workspace } from 'sheets'
+import "./Workspace.css"
 import * as React from 'react'
-import { SheetsTable } from './Table'
+import { renderTable } from './Table';
 
 interface WorkspaceProps {
 	workspace: Workspace
 }
 
 export class SheetsWorkspace extends React.Component<WorkspaceProps> {
+
+	updateCanvas = () => {
+		const canvas = this.refs.canvas as HTMLCanvasElement
+		const ctx = canvas.getContext("2d")
+		if (ctx) {
+			let i = 0
+			for (const table of this.props.workspace.getTables()){
+				renderTable({
+					renderingContext: ctx,
+					table: table,
+					cellHeight: 1,
+					offset: {
+						x: i * 450,
+						y: 0
+					}
+				})
+				i++
+			}
+		}
+	}
+
+	componentDidMount() {
+		this.updateCanvas()
+	}
+
+	componentDidUpdate() {
+		this.updateCanvas()
+	}
+
 	render() {
-		return this.props.workspace.getTables().map(table => {
-			return <SheetsTable table={table} />
-		})
+		console.log('Didd draw canvas')
+		return (
+			<canvas className="Workspace" width={1200} height={1200} ref="canvas" />
+		)
 	}
 }
